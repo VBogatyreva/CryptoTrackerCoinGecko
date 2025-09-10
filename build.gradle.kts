9fd847bc1ecd3bd03d51d2023abd7b04f5b23eec
Viktoria Bogatyreva <bogatyreva-va@mail.ru>
2025-09-10 13:50:35 +0300

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 buildscript {
     repositories {
-        google()
         mavenCentral()
+        google()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.7.2")
         classpath("com.google.gms:google-services:4.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
